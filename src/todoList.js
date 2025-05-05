c4d2a31ecb6dbb2e577800ff35cccc07f21a1bb4
kessius00 <kes.olling00@gmail.com>
2025-05-05 11:04:29 +0200

    add complete c

--- src/todoList.js
+++ src/todoList.js
@@ -1,34 +1,35 @@
 class Todo {
     constructor(title, description, dueDate, priority = 1) {
-      this.title = title;
-      this.description = description;
-      this.dueDate = dueDate;
-      this.priority = priority;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.completed = false;
     }
   }
   
-  class TodoList {
+class TodoList {
     constructor(title) {
       this.title = title;
       this.todos = [];
     }
-  
+
     addTodo(todo) {
-      if (todo instanceof Todo) {
-        this.todos.push(todo);
-      } else {
-        throw new Error("addTodo expects an instance of Todo");
-      }
+        if (todo instanceof Todo) {
+            this.todos.push(todo);
+        } else {
+            throw new Error("addTodo expects an instance of Todo");
+        }
     }
   
     removeTodo(index) {
-      if (index >= 0 && index < this.todos.length) {
-        this.todos.splice(index, 1);
-      }
+        if (index >= 0 && index < this.todos.length) {
+            this.todos.splice(index, 1);
+        }
     }
   
     getTodos() {
-      return this.todos;
+        return this.todos;
     }
 
     randomizeOrder() {
@@ -38,9 +39,15 @@ class Todo {
             let k = this.todos[i];
             this.todos[i] = this.todos[j];
             this.todos[j] = k;
-          }
+        }
     }
-  }
+
+    completeTodo(index) {
+        if (index >= 0 && index < this.todos.length) {
+            this.todos[index].completed = true;
+        }
+    }
+}
   
   export  {Todo, TodoList};
   
